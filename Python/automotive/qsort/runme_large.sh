acd37fee56293bae3f4f123890e33a6057b24f8e
hoseinaghaei <h.aghaei.araei@gmail.com>
2024-03-03 21:10:41 +0330

    create Python language folder.

---
+++ Python/automotive/qsort/runme_large.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python3 qsort_large.py ../../C/automotive/qsort/input_large.dat > output_large.txt
+
---
+++ Python/automotive/qsort/runme_small.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3 qsort_small.py ../../C/automotive/qsort/input_small.dat > output_small.txt
